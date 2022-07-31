b7e2636656862f12c9245ca8a418b905523b56b7
nurjaha123 <109072782+nurjaha123@users.noreply.github.com>
2022-07-31 23:00:35 +0530

    Add files via upload

---
+++ PatternEx.java
@@ -0,0 +1,14 @@
+class PatternEx
+{
+	public static void main (String[] args)
+	{
+		for(int row=1;row<=5;row++)
+		{
+		  for(int column=1;column<=5;column++)
+		   {
+			System.out.print("*");	
+		   }
+		  System.out.println();
+		}
+	}
+}
